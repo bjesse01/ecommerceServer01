3e1802b3d51afe2ab72ee8dc98a3a41c987b33bc
bjesse01 <you@example.combernardsonj01@gmail.com>
2023-09-23 07:39:29 -0700

    first commit

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const admin = require("firebase-admin");
 const serviceAccount = require("./ecommerceapi-25299.json");
+const bodyParser = require("body-parser");
+const db = require("./src/database");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -10,7 +12,66 @@ admin.initializeApp({
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(cors());
+
+//DATABASE CONNECTION
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL: ", err);
+    return;
+  }
+  console.log("Connected to database");
+});
+
+//GENERATE TIME AND DATA
+function generateDateTime() {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const day = String(now.getDay()).padStart(2, "0");
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+
+  const formattedDate = `${day}-${month}-${year}`;
+  const formattedTime = `${hours}:${minutes}:${seconds}`;
+  return { date: formattedDate, time: formattedTime };
+}
+const { date, time } = generateDateTime();
+// console.log(date);
+// console.log(time);
+
+//FOR EMAIL
+app.post("/sendEmail", (req, res) => {
+  const { email, message } = req.body;
+
+  const sqlQuery =
+    "INSERT INTO reviews (date, time, email, message) VALUES (?, ?, ?, ?)";
+  const values = [date, time, email, message];
+
+  db.query(sqlQuery, values, (err) => {
+    if (err) {
+      res.status(500).json({ error: "Failed to save data" });
+    } else {
+      res.status(200).json({ message: "Data saved successfully" });
+    }
+  });
+});
+
+app.get("/mailData", (req, res) => {
+  const { email, message } = req.body;
+
+  const sqlQuery = "SELECT * FROM reviews";
+  db.query(sqlQuery, (err, results) => {
+    if (err) {
+      console.error("Error executing sql query: ", err);
+      res.status(500).json({ error: "Failed to retrieve data" });
+    } else {
+      res.status(200).json(results);
+    }
+  });
+});
 
 // FOR EXPRESS
 app.get("/api/clothings", async (req, res) => {
